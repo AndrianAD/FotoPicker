ad33978c5e6a17711fba82ba2783a8bb0370ae13
Tegritee <Tegritee@tutanota.com>
2019-11-18 11:44:04 +0200

    5

--- fotopickermodule/src/main/java/com/android/fotopickermodule/FotoPicker.kt
+++ fotopickermodule/src/main/java/com/android/fotopickermodule/FotoPicker.kt
@@ -3,9 +3,13 @@ package com.android.fotopickermodule
 import android.app.Activity
 import android.widget.Toast
 
-fun Activity.startPicker(type: String) {
+
+class FotoPicker {
+
+    fun startPicker(activity: Activity, type: String) {
 //    val intent = Intent(this, SecondActivity::class.java)
 //    intent.putExtra(TYPE, type)
 //    this.startActivityForResult(intent, PICK_IMAGE)
-    Toast.makeText(baseContext, "Hello From Photo Picker", Toast.LENGTH_LONG)
+        Toast.makeText(activity, "Hello From Photo Picker", Toast.LENGTH_LONG)
+    }
 }
